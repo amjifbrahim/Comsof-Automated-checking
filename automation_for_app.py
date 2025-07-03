0a1b376290350814f0aaa624f5cde87d5416c229
Abdoelbahli <abdellahelbahli@gmail.com>
2025-07-03 10:50:53 +0100

    Initial commit

--- automation_for_app.py
+++ automation_for_app.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 
 __all__ = ['check_osc_duplicates', 'check_invalid_cable_refs', 'report_splice_counts_by_closure', 'process_shapefiles', 'check_gistool_id', 
-           'check_cluster_overlaps', 'check_granularity_fields', 'validate_non_virtual_closures', 'validate_feeder_primdistribution_locations']
+           'check_cluster_overlaps', 'check_granularity_fields', 'validate_non_virtual_closures', 'validate_feeder_primdistribution_locations','validate_cable_diameters']
 
 
 
